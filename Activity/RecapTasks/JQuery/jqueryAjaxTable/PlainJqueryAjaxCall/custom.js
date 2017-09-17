90e6a2760c267445b7ae89f696ed5ea90443fbeb
Spartan <>
2017-09-17 11:27:13 +0530

    no message

--- Activity/RecapTasks/JQuery/jqueryAjaxTable/PlainJqueryAjaxCall/custom.js
+++ Activity/RecapTasks/JQuery/jqueryAjaxTable/PlainJqueryAjaxCall/custom.js
@@ -8,12 +8,17 @@ $(document).ready(function() {
       format: "json"
     }, function(data) {
       $.each(data, function(index, val) {
-        $("#customtable").append("<tr><th>" + val.id + "</th>" + "<th>" + val.name + "</th><td>" + val.country + "</th><td>" + val.city + "</td><td><button onClick=delete(val)>delete</button></td></tr>");
+        $("#customtable").append("<tr><td>" + val.id + "</td><td>" + val.name + "</td><td>" + val.country + "</td><td>" + val.city + "</td><td><button onClick=remove(val)>Remove</button></td></tr>");
       })
     });
     $("#loadjson").hide();
     $("#deletelane").show();
   });
+});
+
+//Remove Row function
+$(document).ready(function() {
+
 });
 
 //search bar
@@ -35,7 +40,7 @@ $(document).ready(function() {
 //Delete Entire Table
 $(document).ready(function() {
   $('#deletetable').click(function() {
-    if (confirm("Do you want to delete this entire Table?")) {
+    if (confirm("Do you want to close the directory?")) {
       this.click;
       $('#customtable').remove();
     } else {
@@ -49,16 +54,19 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('th').each(function(col) {
     $(this).hover(
-    function() { $(this).addClass('focus'); },
-    function() { $(this).removeClass('focus'); }
-  );
+      function() {
+        $(this).addClass('focus');
+      },
+      function() {
+        $(this).removeClass('focus');
+      }
+    );
     $(this).click(function() {
       if ($(this).is('.asc')) {
         $(this).removeClass('asc');
         $(this).addClass('desc selected');
         sortOrder = -1;
-      }
-      else {
+      } else {
         $(this).addClass('asc selected');
         $(this).removeClass('desc');
         sortOrder = 1;
@@ -69,10 +77,10 @@ $(document).ready(function() {
       arrData.sort(function(a, b) {
         var val1 = $(a).children('td').eq(col).text().toUpperCase();
         var val2 = $(b).children('td').eq(col).text().toUpperCase();
-        if($.isNumeric(val1) && $.isNumeric(val2))
-        return sortOrder == 1 ? val1-val2 : val2-val1;
+        if ($.isNumeric(val1) && $.isNumeric(val2))
+          return sortOrder == 1 ? val1 - val2 : val2 - val1;
         else
-           return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
+          return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
       });
       $.each(arrData, function(index, row) {
         $('tbody').append(row);
