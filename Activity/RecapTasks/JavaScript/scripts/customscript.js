2fcaae61cd2e102726bb67d0d53bf83aa03d9141
santoshofs <santoshebay12@gmail.com>
2017-09-19 17:37:31 +0530

    no message

--- Activity/RecapTasks/JavaScript/scripts/customscript.js
+++ Activity/RecapTasks/JavaScript/scripts/customscript.js
@@ -70,7 +70,7 @@ type = [];
 sno = [];
 cname = [];
 loc = [];
-
+//Company Table
 function companyTable() {
   for (var i = 0; i < (x.data.com).length; i++) {
     sno[i] = x.data.com[i].sno;
@@ -87,11 +87,12 @@ function companyTable() {
       row.insertCell().innerHTML = (x.data.com[i].sno);
       row.insertCell().innerHTML = (x.data.com[i].cname);
       row.insertCell().innerHTML = (x.data.com[i].loc);
-      row.insertCell().innerHTML = "<button type='button' onclick='edit_row()' class='btn btn-sm btn-success'><span class='glyphicon glyphicon-pencil'></span>Edit</button> <button type='button' class='btn btn-sm btn-danger'><span class='glyphicon glyphicon-trash'></span> Delete</button>";
+      var b = (x.data.com[i].sno);
+      row.insertCell().innerHTML = ("<button type='button' class='btn btn-sm btn-success' ><span class='glyphicon glyphicon-pencil'></span></button> <button type='button'  class='btn btn-sm btn-danger' onClick='removecompanyfunc(\"" + (x.data.com[i].sno) + "\");'><span class='glyphicon glyphicon-trash'></span></button>");
     }
   }
 }
-
+//Employee Table
 function empTable() {
   for (var i = 0; i < (x.data.emp).length; i++) {
     sno[i] = x.data.emp[i].sno;
@@ -113,48 +114,45 @@ function empTable() {
   }
 }
 
-function edit_row() {
-      document.getElementById("edit_button" + no).style.display = "none";
-      document.getElementById("save_button" + no).style.display = "block";
-      var name = document.getElementById("name_row" + no);
-      var country = document.getElementById("country_row" + no);
-      var age = document.getElementById("age_row" + no);
-      var name_data = name.innerHTML;
-      var country_data = country.innerHTML;
-      var age_data = age.innerHTML;
-      id.innerHTML = "<input type='text' id='name_text" + no + "' value='" + name_data + "'>";
-      name.innerHTML = "<input type='text' id='country_text" + no + "' value='" + country_data + "'>";
-      type.innerHTML = "<input type='text' id='age_text" + no + "' value='" + age_data + "'>";
+function removecompanyfunc(c) {
+
+  console.log("button" + c);
+  var s = c++;
+  ++s;
+  console.log(s);
+  document.getElementById("companytable").deleteRow(s);
+  //delete x.data.items[s];
+  local.set('value', x.data.com);
+}
+
+//local storage
+var local = (function() {
+  var setData = function(key, obj) {
+    var values = JSON.stringify(obj);
+    localStorage.setItem(key, values);
+  }
+  var getData = function(key) {
+    if (localStorage.getItem(key) != null) {
+      return JSON.parse(localStorage.getItem(key));
+    } else {
+      return false;
     }
-
-    function save_row(no) {
-      var name_val = document.getElementById("name_text" + no).value;
-      var country_val = document.getElementById("country_text" + no).value;
-      var age_val = document.getElementById("age_text" + no).value;
-
-      document.getElementById("name_row" + no).innerHTML = name_val;
-      document.getElementById("country_row" + no).innerHTML = country_val;
-      document.getElementById("age_row" + no).innerHTML = age_val;
-
-      document.getElementById("edit_button" + no).style.display = "block";
-      document.getElementById("save_button" + no).style.display = "none";
+  }
+  var updateDate = function(key, newData) {
+    if (localStorage.getItem(key) != null) {
+      var oldData = JSON.parse(localStorage.getItem(key));
+      for (keyObj in newData) {
+        oldData[keyObj] = newData[keyObj];
+      }
+      var values = JSON.stringify(oldData);
+      localStorage.setItem(key, values);
+    } else {
+      return false;
     }
-
-    function delete_row(no) {
-      document.getElementById("row" + no + "").outerHTML = "";
-    }
-
-    function add_row() {
-      var new_name = document.getElementById("new_name").value;
-      var new_country = document.getElementById("new_country").value;
-      var new_age = document.getElementById("new_age").value;
-      var table = document.getElementById("data_table");
-      var table_len = (table.rows.length) - 1;
-      var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='name_row" + table_len + "'>" + new_name + "</td><td id='country_row" + table_len + "'>" + new_country + "</td><td id='age_row" + table_len + "'>" + new_age +
-        "</td><td><input type='button' id='edit_button" + table_len + "' value='Edit' class='edit' onclick='edit_row(" + table_len + ")'> <input type='button' id='save_button" + table_len + "' value='Save' class='save' onclick='save_row(" +
-        table_len + ")'> <input type='button' value='Delete' class='delete' onclick='delete_row(" + table_len + ")'></td></tr>";
-
-      document.getElementById("new_name").value = "";
-      document.getElementById("new_country").value = "";
-      document.getElementById("new_age").value = "";
-    }
+  }
+  return {
+    set: setData,
+    get: getData,
+    update: updateDate
+  }
+})();
