a7a824c1649a2e0039711d204847fd86468ca370
santoshofs <santoshebay12@gmail.com>
2017-09-19 16:58:01 +0530

    no message

--- Activity/RecapTasks/Angularjs/app/app.js
+++ Activity/RecapTasks/Angularjs/app/app.js
@@ -1,14 +1,14 @@
 var app = angular.module('angularTable', ['angularUtils.directives.dirPagination']);
 
-app.controller('listdata',function($scope, $http){
-	$scope.users = []; //declare an empty array
-	$http.get("https://api.myjson.com/bins/62lc5").success(function(response){
-		$scope.users = response;  //ajax request to fetch data into $scope.data
-		$scope.sortBy = 'emp_name';
-	});
+app.controller('listdata', function($scope, $http) {
+  $scope.users = []; //declare an empty array
+  $http.get("https://api.myjson.com/bins/62lc5").success(function(response) {
+    $scope.users = response; //ajax request to fetch data into $scope.data
+    $scope.sortBy = 'emp_name';
+  });
 
-	$scope.sort = function(keyname){
-		$scope.sortKey = keyname;   //set the sortKey to the param passed
-		$scope.reverse = !$scope.reverse; //if true make it false and vice versa
-	}
+  $scope.sort = function(keyname) {
+    $scope.sortKey = keyname; //set the sortKey to the param passed
+    $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+  }
 });
