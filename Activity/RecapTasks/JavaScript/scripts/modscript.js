1339d55a0de5ffb40121f70d625af83d2bde103a
spartaa <spartaaa@gmail.com>
2017-09-22 17:48:19 +0530

    no message

--- Activity/RecapTasks/JavaScript/scripts/modscript.js
+++ Activity/RecapTasks/JavaScript/scripts/modscript.js
@@ -65,7 +65,7 @@ var companyobjects = [{
 var companyjsondata = [];
 var employeejsondata = [];
 
-//passing json obj to the function
+//passing json obj comcomeditorcommandsn
 localinitcompanydata(companyobjects);
 localinitemployeedata(employeeobjcts);
 
@@ -94,22 +94,55 @@ function feedemployeedata() {
 }
 
 function loadcompanytable() {
-  document.getElementById("editcominput").style.display = "none";
+  document.getElementById("companyeditor").style.display = "none";
+  document.getElementById("newcompanyeditor").style.display = "none";
   var companytable = '<tr></tr>';
   for (var i = 0; i < companyjsondata.length; i++) {
-    companytable += "<tr><td>" + companyjsondata[i].sno + "</td><td>" + companyjsondata[i].cname + "</td><td>" + companyjsondata[i].loc + "</td><td><button type='button' class='btn btn-sm btn-success' onclick='editcompanyrecord(" + i + ")'><span class='glyphicon glyphicon-pencil'></span> Edit</button><button type='button'  class='btn btn-sm btn-danger' onclick='deleteCompanyRecord(" + i + ")'><span class='glyphicon glyphicon-trash'></span> Delete</button></td>";
+    companytable += "<tr><td>" + companyjsondata[i].sno + "</td><td>" + companyjsondata[i].cname + "</td><td>" + companyjsondata[i].loc + "</td><td><button type='button' class='btn btn-sm btn-success' onclick='editcompanyrecord(" + i + ")'><span class='glyphicon glyphicon-pencil'></span> Edit</button> <button type='button'  class='btn btn-sm btn-danger' onclick='removecomany(" + i + ")'><span class='glyphicon glyphicon-trash'></span> Delete</button></td>";
   }
   //  var newcomrecord = '<input type="button" value="create" onclick="createRecordCm()">';
   var newcomrecord = document.getElementById('addnewcomrecord');
   document.getElementById("companytable").innerHTML = companytable;
-  document.getElementById("newcominput").innerHTML = newcomrecord;
+  // document.getElementById("newcominput").innerHTML = newcomrecord;
 }
-function editcompanyrecord(d){
-  document.getElementById("editcominput").style.display = "block";
+
+function editcompanyrecord(ce) {
+  document.getElementById("companyeditor").style.display = "block";
   // document.getElementById('text6').value =companyjsondata[d].cname;
   // var companyeditarea = '<form><input id="text7" >' +  '</input><textarea id="text7">' + companyjsondata[d].cname + '</textarea><textarea id="text8">' + companyjsondata[d].loc + '</textarea><button onclick="updateEditCompany(' + d + ')">update</button><button  onClick="editDataCompany(' + d + ')">Reset</button><button  onClick="cancelCm()">Cancel</button></form>';
   // document.getElementById("companyeditspace").innerHTML = companyeditarea;
-  document.getElementById('comidform').value =companyjsondata[d].sno;
-  document.getElementById('comnameform').value =companyjsondata[d].cname;
-  document.getElementById('composform').value =companyjsondata[d].loc;
+  document.getElementById('comidform').value = companyjsondata[ce].sno;
+  document.getElementById('comnameform').value = companyjsondata[ce].cname;
+  document.getElementById('composform').value = companyjsondata[ce].loc;
+  var editclicks = '<button type="submit" class="btn btn-sm btn-success" id="comsubmitform" onclick="updatecom(' + ce + ')">update</button> <button class="btn btn-sm btn-danger" id="comeditcancel" onclick="cancelempeditor()">Cancel</button>';
+  document.getElementById("comeditorcommands").innerHTML = editclicks;
+}
+
+function updatecom(uc) {
+  document.getElementById("companyeditor").style.display = "none";
+  companyjsondata[uc].sno = document.getElementById("comidform").value;
+  companyjsondata[uc].cname = document.getElementById("comnameform").value;
+  companyjsondata[uc].loc = document.getElementById("composform").value;
+  localinitcompanydata(companyjsondata);
+  loadcompanytable();
+}
+
+function cancelempeditor() {
+  document.getElementById("companyeditor").style.display = "none";
+}
+
+function removecomany(rc) {
+  companyjsondata.splice(rc, 1);
+  localinitcompanydata(companyjsondata);
+  loadcompanytable();
+}
+
+function addnewcom() {
+  document.getElementById("newcompanyeditor").style.display = "block";
+  var neweditclicks = '<button class="btn btn-sm btn-success" id="comsubmitform" onclick="savenewcom()">Submit</button> <button class="btn btn-sm btn-danger" id="comeditcancel" onclick="cancelnewempeditor()">Cancel</button>';
+  document.getElementById("newcomeditorcommands").innerHTML = neweditclicks;
+}
+
+function cancelnewempeditor() {
+  document.getElementById("newcompanyeditor").style.display = "none";
 }
