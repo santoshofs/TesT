2cc9f22799eddd65385e0064c520e7603fa2b09e
santoshofs <santoshebay12@gmail.com>
2017-09-22 12:31:15 +0530

    no message

---
+++ Activity/RecapTasks/JavaScript/scripts/modscript.js
@@ -0,0 +1,105 @@
+var employeeobjcts = [{
+    "id": "1",
+    "name": "Santosh",
+    "type": "Legend"
+  },
+  {
+    "id": "2",
+    "name": "Kaba",
+    "type": "Ultra Legend"
+  },
+  {
+    "id": "3",
+    "name": "Munishkanth",
+    "type": "Alien Level"
+  },
+  {
+    "id": "4",
+    "name": "Alia",
+    "type": "kid"
+  },
+  {
+    "id": "5",
+    "name": "Shraddha",
+    "type": "legend"
+  },
+  {
+    "id": "6",
+    "name": "DocStrange",
+    "type": "kid"
+  }
+];
+
+var companyobjects = [{
+    "sno": "1",
+    "cname": "SanTech",
+    "loc": "Chennai"
+  },
+  {
+    "sno": "2",
+    "cname": "Google",
+    "loc": "Banglore"
+  },
+  {
+    "sno": "3",
+    "cname": "EA",
+    "loc": "New-York"
+  },
+  {
+    "sno": "4",
+    "cname": "Action",
+    "loc": "Japan"
+  },
+  {
+    "sno": "5",
+    "cname": "Ubisoft",
+    "loc": "Pune"
+  },
+  {
+    "sno": "6",
+    "cname": "Gameloft",
+    "loc": "Delhi"
+  }
+];
+
+var companyjsondata = [];
+var employeejsondata = [];
+
+//passing json obj to the function
+localinitcompanydata(companyobjects);
+localinitemployeedata(employeeobjcts);
+
+//init local storage for company
+function localinitcompanydata(comdata) {
+  var comvalue = JSON.stringify(comdata);
+  localStorage.setItem('companydatum', comvalue);
+  feedcompanydata();
+}
+//load the company data from local storage
+function feedcompanydata() {
+  var companydatatemp = localStorage.getItem("companydatum");
+  companyjsondata = JSON.parse(companydatatemp);
+}
+
+//init local storage for employee
+function localinitemployeedata(empdata) {
+  var empvalue = JSON.stringify(empdata);
+  localStorage.setItem('employeedatum', empvalue);
+  feedemployeedata();
+}
+//load the employee data from local storage
+function feedemployeedata() {
+  var employeedatatemp = localStorage.getItem("employeedatum");
+  employeejsondata = JSON.parse(employeedatatemp);
+}
+
+function loadcompanytable() {
+   var companytable ='<tr></tr>';
+   for (var i = 0; i < companyjsondata.length; i++) {
+     companytable += "<tr><td>" + companyjsondata[i].sno + "</td><td>" + companyjsondata[i].cname + "</td><td>" + companyjsondata[i].loc + "</td><td><button type='button' class='btn btn-sm btn-success' onclick='editDataCompany(" + i + ")'><span class='glyphicon glyphicon-pencil'></span> Edit</button><button type='button'  class='btn btn-sm btn-danger' onclick='deleteCompanyRecord(" + i + ")'><span class='glyphicon glyphicon-trash'></span> Delete</button></td>";
+}
+  //  var record = '<input type="button" value="create" onclick="createRecordCm()">';
+    var record = document.getElementById('addnewcomrecord');
+   document.getElementById("companytable").innerHTML = companytable;
+   document.getElementById("newcominput").innerHTML = record;
+ }
