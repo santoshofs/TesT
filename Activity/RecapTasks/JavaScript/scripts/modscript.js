f82bd81290d3d7c4fd496ca49d52669b876e4911
Spartan <>
2017-09-24 09:26:19 +0530

    no message

--- Activity/RecapTasks/JavaScript/scripts/modscript.js
+++ Activity/RecapTasks/JavaScript/scripts/modscript.js
@@ -100,17 +100,13 @@ function loadcompanytable() {
   for (var i = 0; i < companyjsondata.length; i++) {
     companytable += "<tr><td>" + companyjsondata[i].sno + "</td><td>" + companyjsondata[i].cname + "</td><td>" + companyjsondata[i].loc + "</td><td><button type='button' class='btn btn-sm btn-success' onclick='editcompanyrecord(" + i + ")'><span class='glyphicon glyphicon-pencil'></span> Edit</button> <button type='button'  class='btn btn-sm btn-danger' onclick='removecomany(" + i + ")'><span class='glyphicon glyphicon-trash'></span> Delete</button></td>";
   }
-  //  var newcomrecord = '<input type="button" value="create" onclick="createRecordCm()">';
   var newcomrecord = document.getElementById('addnewcomrecord');
   document.getElementById("companytable").innerHTML = companytable;
-  // document.getElementById("newcominput").innerHTML = newcomrecord;
 }
 
 function editcompanyrecord(ce) {
   document.getElementById("companyeditor").style.display = "block";
-  // document.getElementById('text6').value =companyjsondata[d].cname;
-  // var companyeditarea = '<form><input id="text7" >' +  '</input><textarea id="text7">' + companyjsondata[d].cname + '</textarea><textarea id="text8">' + companyjsondata[d].loc + '</textarea><button onclick="updateEditCompany(' + d + ')">update</button><button  onClick="editDataCompany(' + d + ')">Reset</button><button  onClick="cancelCm()">Cancel</button></form>';
-  // document.getElementById("companyeditspace").innerHTML = companyeditarea;
+  document.getElementById("newcompanyeditor").style.display = "none";
   document.getElementById('comidform').value = companyjsondata[ce].sno;
   document.getElementById('comnameform').value = companyjsondata[ce].cname;
   document.getElementById('composform').value = companyjsondata[ce].loc;
@@ -139,10 +135,92 @@ function removecomany(rc) {
 
 function addnewcom() {
   document.getElementById("newcompanyeditor").style.display = "block";
-  var neweditclicks = '<button class="btn btn-sm btn-success" id="comsubmitform" onclick="savenewcom()">Submit</button><button class="btn btn-sm btn-danger" id="comeditcancel" onclick="cancelnewempeditor()">Cancel</button>';
+  document.getElementById("companyeditor").style.display = "none";
+  var neweditclicks = '<button class="btn btn-sm btn-success" id="comsubmitform" onclick="savenewcom()">Submit</button> <button class="btn btn-sm btn-danger" id="comeditcancel" onclick="cancelnewempeditor()">Cancel</button>';
   document.getElementById("newcomeditorcommands").innerHTML = neweditclicks;
 }
 
 function cancelnewempeditor() {
   document.getElementById("newcompanyeditor").style.display = "none";
+}
+
+function savenewcom() {
+  var tempsno = document.getElementById("newcomidform").value;
+  var tempname = document.getElementById("newcomnameform").value;
+  var temploc = document.getElementById("newcomposform").value;
+  var newcomdata = {
+    "sno": tempsno,
+    "cname": tempname,
+    "loc": temploc
+  };
+  companyjsondata.push(newcomdata);
+  localinitcompanydata(companyjsondata);
+  loadcompanytable();
+}
+
+//emp
+
+function loademployeetable() {
+  document.getElementById("employeeeditor").style.display = "none";
+  document.getElementById("newemployeeeditor").style.display = "none";
+  var employeetable = '<tr></tr>';
+  for (var i = 0; i < employeejsondata.length; i++) {
+    employeetable += "<tr><td>" + employeejsondata[i].id + "</td><td>" + employeejsondata[i].name + "</td><td>" + employeejsondata[i].type + "</td><td><button type='button' class='btn btn-sm btn-success' onclick='editemployeerecord(" + i + ")'><span class='glyphicon glyphicon-pencil'></span> Edit</button> <button type='button'  class='btn btn-sm btn-danger' onclick='removeemployee(" + i + ")'><span class='glyphicon glyphicon-trash'></span> Delete</button></td>";
+  }
+  var newemprecord = document.getElementById('addnewemprecord');
+  document.getElementById("employeetable").innerHTML = employeetable;
+}
+
+function editemployeerecord(ce) {
+  document.getElementById("employeeeditor").style.display = "block";
+  document.getElementById("newemployeeeditor").style.display = "none";
+  document.getElementById('empidform').value = employeejsondata[ce].id;
+  document.getElementById('empnameform').value = employeejsondata[ce].name;
+  document.getElementById('empposform').value = employeejsondata[ce].type;
+  var editclicks = '<button type="submit" class="btn btn-sm btn-success" id="empsubmitform" onclick="updateemp(' + ce + ')">Update</button> <button class="btn btn-sm btn-danger" id="empeditcancel" onclick="cancelempeditor()">Cancel</button>';
+  document.getElementById("empeditorcommands").innerHTML = editclicks;
+}
+
+function updateemp(uc) {
+  document.getElementById("employeeeditor").style.display = "none";
+  employeejsondata[uc].id = document.getElementById("empidform").value;
+  employeejsondata[uc].name = document.getElementById("empnameform").value;
+  employeejsondata[uc].type = document.getElementById("empposform").value;
+  localinitemployeedata(employeejsondata);
+  loademployeetable();
+}
+
+function cancelempeditor() {
+  document.getElementById("employeeeditor").style.display = "none";
+}
+
+function removeemployee(rc) {
+  employeejsondata.splice(rc, 1);
+  localinitemployeedata(employeejsondata);
+  loademployeetable();
+}
+
+function addnewemp() {
+  document.getElementById("newemployeeeditor").style.display = "block";
+  document.getElementById("employeeeditor").style.display = "none";
+  var neweditclicks = '<button class="btn btn-sm btn-success" id="empsubmitform" onclick="savenewemp()">Submit</button> <button class="btn btn-sm btn-danger" id="empeditcancel" onclick="cancelnewempeditor()">Cancel</button>';
+  document.getElementById("newempeditorcommands").innerHTML = neweditclicks;
+}
+
+function cancelnewempeditor() {
+  document.getElementById("newemployeeeditor").style.display = "none";
+}
+
+function savenewemp() {
+  var tempid = document.getElementById("newempidform").value;
+  var tempname = document.getElementById("newempnameform").value;
+  var temptype = document.getElementById("newempposform").value;
+  var newempdata = {
+    "id": tempid,
+    "name": tempname,
+    "type": temptype
+  };
+  employeejsondata.push(newempdata);
+  localinitemployeedata(employeejsondata);
+  loademployeetable();
 }
