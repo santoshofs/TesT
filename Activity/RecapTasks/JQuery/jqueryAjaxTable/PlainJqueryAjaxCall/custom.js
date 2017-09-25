7fda5badadfb343e7231e4ee8c6cee33afcebfb9
spartaa <spartaaa@gmail.com>
2017-09-25 17:49:49 +0530

    no message

--- Activity/RecapTasks/JQuery/jqueryAjaxTable/PlainJqueryAjaxCall/custom.js
+++ Activity/RecapTasks/JQuery/jqueryAjaxTable/PlainJqueryAjaxCall/custom.js
@@ -35,6 +35,24 @@ $(document).ready(function() {
   })
 });
 
+//table search
+function searchintable() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("customtable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 //Delete Entire Table
 $(document).ready(function() {
   $('#deletetable').click(function() {
