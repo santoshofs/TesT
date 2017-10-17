40203ef6173323265cfd514b2efdb11fdd8b3f22
MunizKanth <muniz@papampatti.in>
2017-10-17 21:04:48 +0530

    no message

--- Activity/Mvp/LoginUI/script.js
+++ Activity/Mvp/LoginUI/script.js
@@ -1,4 +1,6 @@
-alert("Js Link test");
+
 // $('#myModal').then('show');
-var app = angular.module('signupmodule', []);
-app.controller('signupcontroller', function($scope) {});
+var app = angular.module('usermodule', []);
+app.controller('usercontroller', function($scope) {
+
+});
