afa3b67a00accf2b6b9d2f818423c84c774611e9
spartaa <spartaaa@gmail.com>
2017-10-20 14:56:15 +0530

    no message

---
+++ Activity/MVP Refferal/ArulLogin/loginmodule/src/com/loginmodule/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.loginmodule.service;
+
+import com.loginmodule.dao.UserDao;
+import com.loginmodule.dao.UserDaoImpl;
+import com.loginmodule.model.User;
+
+public class UserServiceImpl implements UserService{
+	
+	UserDao userDao = new UserDaoImpl();
+
+	@Override
+	public User findUserByEmail(String email) {
+	
+		return userDao.findUserByEmail(email);
+	}
+
+}
