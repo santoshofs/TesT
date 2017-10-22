a86dc62ac89517a4da20e924e99ec4f9b547c8ec
MunizKanth <muniz@papampatti.in>
2017-10-22 23:41:40 +0530

    no message

--- Activity/MVP Refferal/UImenu/scripts/app.js
+++ Activity/MVP Refferal/UImenu/scripts/app.js
@@ -10,6 +10,25 @@ $(function() {
   });
 });
 
+$(document).ready(function(){
+    $("#accountcheckin").click(function(){
+        $("#usercheckinModal").modal();
+        $("#login-inner-nav").addClass("active");
+        $("#register-inner-nav").removeClass("active");
+    });
+    $("#logintab").click(function(){
+        $("#usercheckinModal").modal();
+        $("#login-inner-nav").addClass("active");
+        $("#register-inner-nav").removeClass("active");
+    });
+    $("#registertab").click(function(){
+        $("#usercheckinModal").modal();
+        $("#register-inner-nav").addClass("active");
+        $("#login-inner-nav").removeClass("active");
+    });
+
+});
+
 var SanTechApp = angular.module('SanTechApp', ["ui.router"]);
 
 SanTechApp.config(function($stateProvider, $urlRouterProvider) {
