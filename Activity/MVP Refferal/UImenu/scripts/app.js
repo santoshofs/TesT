4af79bf8297e1b1bbdfd5ec83dcb43d24a25b943
MunizKanth <muniz@papampatti.in>
2017-10-22 22:51:54 +0530

    no message

--- Activity/MVP Refferal/UImenu/scripts/app.js
+++ Activity/MVP Refferal/UImenu/scripts/app.js
@@ -1,1 +1,36 @@
+$(function() {
+  $("#hometag").addClass("active");
+  $("#brandhear").on("click", function() {
+    $(".nav li").removeClass("active");
+    $("#hometag").addClass("active");
+  });
+  $(".nav li").on("click", function() {
+    $(".nav li").removeClass("active");
+    $(this).addClass("active");
+  });
+});
 
+var SanTechApp = angular.module('SanTechApp', ["ui.router"]);
+
+SanTechApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state("home", {
+            url: "/home",
+            templateUrl: 'views/home.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit
+        })
+        .state("user", {
+            url: "/user",
+            templateUrl: 'views/usercheckin.html'
+        });
+
+});
