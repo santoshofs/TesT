6a1059d89e8bf349893071fab63005610e40abb4
spartaa <spartaaa@gmail.com>
2017-10-23 15:12:41 +0530

    no message

--- Activity/MVP Refferal/UImenu/scripts/app.js
+++ Activity/MVP Refferal/UImenu/scripts/app.js
@@ -12,63 +12,63 @@ $(function() {
 });
 
 // user checkin credentials
-$(document).ready(function(){
-    $("#accountcheckin").click(function(){
-        $("#usercheckinModal").modal();
-        $("#login-inner-nav").addClass("active");
-        $("#register-inner-nav").removeClass("active");
-        $("#registerForm").removeClass("active");
-        $("#LoginForm").addClass("active");
-    });
-    $("#logintab").click(function(){
-        $("#usercheckinModal").modal();
-        $("#login-inner-nav").addClass("active");
-        $("#register-inner-nav").removeClass("active");
-        $("#registerForm").removeClass("active");
-        $("#LoginForm").addClass("active");
-    });
-    $("#login-inner-nav").click(function(){
-        $("#usercheckinModal").modal();
-        $("#login-inner-nav").addClass("active");
-        $("#register-inner-nav").removeClass("active");
-        $("#registerForm").removeClass("active");
-        $("#LoginForm").addClass("active");
-    });
-    $("#registertab").click(function(){
-        $("#usercheckinModal").modal();
-        $("#register-inner-nav").addClass("active");
-        $("#login-inner-nav").removeClass("active");
-        $("#LoginForm").removeClass("active");
-        $("#registerForm").addClass("active");
-    });
-    $("#register-inner-nav").click(function(){
-        $("#usercheckinModal").modal();
-        $("#register-inner-nav").addClass("active");
-        $("#login-inner-nav").removeClass("active");
-        $("#LoginForm").removeClass("active");
-        $("#registerForm").addClass("active");
-    });
+$(document).ready(function() {
+  $("#accountcheckin").click(function() {
+    $("#usercheckinModal").modal();
+    $("#login-inner-nav").addClass("active");
+    $("#register-inner-nav").removeClass("active");
+    $("#registerForm").removeClass("active");
+    $("#LoginForm").addClass("active");
+  });
+  $("#logintab").click(function() {
+    $("#usercheckinModal").modal();
+    $("#login-inner-nav").addClass("active");
+    $("#register-inner-nav").removeClass("active");
+    $("#registerForm").removeClass("active");
+    $("#LoginForm").addClass("active");
+  });
+  $("#login-inner-nav").click(function() {
+    $("#usercheckinModal").modal();
+    $("#login-inner-nav").addClass("active");
+    $("#register-inner-nav").removeClass("active");
+    $("#registerForm").removeClass("active");
+    $("#LoginForm").addClass("active");
+  });
+  $("#registertab").click(function() {
+    $("#usercheckinModal").modal();
+    $("#register-inner-nav").addClass("active");
+    $("#login-inner-nav").removeClass("active");
+    $("#LoginForm").removeClass("active");
+    $("#registerForm").addClass("active");
+  });
+  $("#register-inner-nav").click(function() {
+    $("#usercheckinModal").modal();
+    $("#register-inner-nav").addClass("active");
+    $("#login-inner-nav").removeClass("active");
+    $("#LoginForm").removeClass("active");
+    $("#registerForm").addClass("active");
+  });
 
 });
 
-// ui routing
+//angularjs Part
 var SanTechApp = angular.module('SanTechApp', ["ui.router"]);
 
+// ui routing
 SanTechApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
+  $stateProvider
 
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state("home", {
-            url: "/home",
-            templateUrl: 'views/home.html'
-        })
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state("home", {
+      url: "/home",
+      templateUrl: 'views/home.html'
+    })
 
-        .state("about", {
-            url: "/about",
-            templateUrl: 'views/about.html'
-        });
-
+    .state("about", {
+      url: "/about",
+      templateUrl: 'views/about.html'
+    });
 });
