e2f90a2dbdc229fd146d9961556f33347a2f008d
MunizKanth <muniz@papampatti.in>
2017-10-30 00:29:41 +0530

    angular_table_tests

---
+++ Activity/Mvp/ui/Angularjs/app/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('angularTable', []);
+// 'angularUtils.directives.dirPagination'
+
+app.controller('listdata', function($scope, $http) {
+  $scope.flights = []; //declare an empty array
+  $http.get("https://api.myjson.com/bins/1gji9n").then(function(response) {
+    $scope.flights = response.data.flights; //ajax request to fetch data into $scope.data
+  });
+  // app.controller('listdata', function($scope, $http) {
+  //     $http.get("https://api.myjson.com/bins/12704r")
+  //     .then(function (response) {$scope.flights = response.data.flights;});
+  // });
+  $scope.sort = function(keyname) {
+    $scope.sortKey = keyname; //set the sortKey to the param passed
+    $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+  }
+});
