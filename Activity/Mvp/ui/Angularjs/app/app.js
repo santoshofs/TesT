aeed1541490349665e1b96a984a1da8ee6f2b2c1
MunizKanth <muniz@papampatti.in>
2017-10-30 09:41:38 +0530

    no message

--- Activity/Mvp/ui/Angularjs/app/app.js
+++ Activity/Mvp/ui/Angularjs/app/app.js
@@ -3,11 +3,12 @@ var app = angular.module('angularTable', []);
 
 app.controller('listdata', function($scope, $http) {
   $scope.flights = []; //declare an empty array
-  $http.get("https://api.myjson.com/bins/1gji9n").then(function(response) {
+  $http.get("http://localhost:8080/FinalMongoAttempt/SanService/control/getAllFlights").then(function(response) {
     $scope.flights = response.data.flights; //ajax request to fetch data into $scope.data
   });
   // app.controller('listdata', function($scope, $http) {
-  //     $http.get("https://api.myjson.com/bins/12704r")
+  //     $http.get("https://api.myjson.com/bins/12704r https://api.myjson.com/bins/1gji9n")
+
   //     .then(function (response) {$scope.flights = response.data.flights;});
   // });
   $scope.sort = function(keyname) {
