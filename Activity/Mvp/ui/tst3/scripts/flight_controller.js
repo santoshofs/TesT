f29f144aa4868b9b1adf0dc78dcc3712c47c1fb7
spartaa <spartaaa@gmail.com>
2017-11-02 17:39:46 +0530

    no message

--- Activity/Mvp/ui/tst3/scripts/flight_controller.js
+++ Activity/Mvp/ui/tst3/scripts/flight_controller.js
@@ -1,27 +1,18 @@
 SanTechApp.controller('flightsearchCtrl2', function($scope, $state, $rootScope, $filter, flightServices) {
-    $scope.travellocation = ["CHENNAI", "DELHI", "MUMBAI", "KOLKATA"];
-    $scope.flight_from;
-    $scope.flight_to;
-    $rootScope.travelDate;
+  $scope.travellocation = ["CHENNAI", "DELHI", "MUMBAI", "KOLKATA"];
+  $scope.flight_from;
+  $scope.flight_to;
+  $rootScope.travelDate;
 
-
-    $scope.searchflight = function() {
-      $rootScope.travelDate = $scope.travel_date;
-      flightServices.checkFlight($scope.flight_from, $scope.flight_to);
-    }
-    $scope.sort = function(keyname) {
-      $scope.sortKey = keyname; //set the sortKey to the param passed
-      $scope.reverse = !$scope.reverse; //if true make it false and vice versa
-    }
-    $scope.bookFlight = function(flight) {
-    //   $scope.date = $filter("date")($scope.travel_date, 'dd-MM-yyyy');
-    // alert($scope.date);
-    //$rootScope.travel_date;
-      // var tdate=$scope.travel_date;
-      // $scope.date = $filter("date")($scope.travel_date, 'dd-MM-yyyy');
-      // alert(tdate);
-      // var travel_date=document.getElementById('travel_date').value;
-      // alert(travel_date);
-      flightServices.bookTicket(flight);
-    }
-  })
+  $scope.searchflight = function() {
+    $rootScope.travelDate = $scope.travel_date;
+    flightServices.checkFlight($scope.flight_from, $scope.flight_to);
+  }
+  $scope.sort = function(keyname) {
+    $scope.sortKey = keyname; //set the sortKey to the param passed
+    $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+  }
+  $scope.bookFlight = function(flight) {
+    flightServices.bookTicket(flight);
+  }
+})
