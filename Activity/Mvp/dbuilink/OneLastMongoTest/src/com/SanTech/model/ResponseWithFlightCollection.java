91e083bfe4fc4419b7e9a95dd646ab742b61d5f2
spartaa <spartaaa@gmail.com>
2017-11-02 10:52:55 +0530

    no message

--- Activity/Mvp/dbuilink/OneLastMongoTest/src/com/SanTech/model/ResponseWithFlightCollection.java
+++
@@ -1,9 +0,0 @@
-package com.SanTech.model;
-
-public class ResponseWithFlightCollection {
-	public String status;
-	public FlightModel[] flights ;
-	public FlightModel flight = new FlightModel();
-	public int rows;
-
-}
--- Activity/Mvp/dbuilink/OneLastMongoTest/src/com/SanTech/model/UserResponse.java
+++
@@ -1,7 +0,0 @@
-package com.SanTech.model;
-
-public class UserResponse {
-	public String status = new String();
-	public UserModel user = new UserModel();
-
-}
